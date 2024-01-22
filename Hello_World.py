69c9f6a7266d71c069f2d2af752e24d2fd015c2b
Daniel Carty <dan120.c1e@gmail.com>
2024-01-22 00:05:42 +0000

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,11 @@
+# Get the name of the user then display it.
+name = input("Enter your name:  ")
+print(name)
+print()
+# Get user's age.
+age = input("Enter your age: ")
+# Display the age.
+print(age)
+print()
+# Display 'Hello world!'
+print("Hello World!")
